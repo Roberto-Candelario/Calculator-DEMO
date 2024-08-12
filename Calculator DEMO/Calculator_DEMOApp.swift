1a24e6d097a9db6a93042cd2fb50406cbc17bb60
Roberto Candelario <r-jc@live.com>
2024-08-11 22:51:26 -0400

    Initial Commit

---
+++ Calculator DEMO/Calculator_DEMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_DEMOApp.swift
+//  Calculator DEMO
+//
+//  Created by Roberto Candelario on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_DEMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator DEMOTests/Calculator_DEMOTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_DEMOTests.swift
+//  Calculator DEMOTests
+//
+//  Created by Roberto Candelario on 8/11/24.
+//
+
+import Testing
+@testable import Calculator_DEMO
+
+struct Calculator_DEMOTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
